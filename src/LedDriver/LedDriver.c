9938b81a2b91eadd073ee1bfe23ba2b16fd2e772
gthediscoking <discoking@ymail.com>
2017-02-05 12:18:47 +0900

    Refactoring: #Book p.57 (with not unity but CppUTest)

--- src/LedDriver/LedDriver.c
+++ src/LedDriver/LedDriver.c
@@ -1,9 +1,12 @@
 #include "LedDriver.h"
 
+// internal variables and functions
 static uint16_t * ledsAddress;
+static uint16_t convertLedNumberToBit(int ledNumber);
+
 void LedDriver_Create(uint16_t * address)
 {
-//	*address = 0;
+//	*address = 0;	// hard coat
 	ledsAddress = address;	// アドレスを設定
 	*ledsAddress = 0;		// LEDを全てOFF
 }
@@ -14,11 +17,18 @@ void LedDriver_Destroy(void)
 
 void LedDriver_TurnOn(int ledNumber)
 {
-	*ledsAddress |= 1 << (ledNumber-1);
+//	*ledsAddress = 1;	// hard coat
+	*ledsAddress |= convertLedNumberToBit(ledNumber);	//LED制御レジスタに対するbit	操作
 }
 
 void LedDriver_TurnOff(int ledNumber)
 {
-//	*ledsAddress = 0;
-	*ledsAddress &= ~(1 << (ledNumber-1));
+//	*ledsAddress = 0;	// hard coat
+	*ledsAddress &= ~(convertLedNumberToBit(ledNumber));	//LED制御レジスタに対するbit	操作
+}
+
+static uint16_t convertLedNumberToBit(int ledNumber)
+{
+	// int型引数を16bit値に変換して返す
+	return (uint16_t)(1 << (ledNumber-1));
 }
